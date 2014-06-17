3dbc0ca382c8509ca22f282087e676f5f2d8711b
David Amanshia <phpengine@hotmail.co.uk>
2014-06-17 10:01:35 +0100

    mysql galera config module

--- src/Modules/MysqlServerGalera/Model/MysqlServerGaleraConfigUbuntu.php
+++ src/Modules/MysqlServerGalera/Model/MysqlServerGaleraConfigUbuntu.php
@@ -14,8 +14,8 @@ class MysqlServerGaleraConfigUbuntu extends Base {
     // Model Group
     public $modelGroup = array("GaleraConfig") ;
 
-    private $environments ;
-    private $environmentReplacements ;
+    protected $environments ;
+    protected $environmentReplacements ;
 
     public function __construct($params) {
       parent::__construct($params);
@@ -49,39 +49,47 @@ class MysqlServerGaleraConfigUbuntu extends Base {
         $this->environments = $environmentConfigModel->environments ;
     }
 
-    public function getServerArrayText($serversArray) {
-        $serversText = "";
-        foreach($serversArray as $serverArray) {
-            $serversText .= 'array(';
-            $serversText .= '"target" => "'.$serverArray["target"].'", ';
-            $serversText .= '"user" => "'.$serverArray["user"].'", ';
-            $serversText .= '"pword" => "'.$serverArray["password"].'", ';
-            $serversText .= '),'."\n"; }
-        return $serversText;
+    public function getClusterStarter($serversArray) {
+        return $serversArray[0]["target"];
     }
 
-    private function doConfigGalera() {
-      if ($this->params["route"]["action"] == "config-cluster-starter") {
+    public function getServersFromEnvName($envName) {
+        foreach ($this->environments as $environment) {
+            if ($environment["any-app"]["gen_env_name"] == $envName) {
+                return $environment["servers"] ; } }
+        return null ;
+    }
+
+    private function askForEnvironment() {
+        $question = 'What is the environment name you want to balance load to? ';
+        $input = self::askForInput($question, true);
+        return $input ;
+    }
+
+    protected function doConfigGalera() {
+        if ($this->params["route"]["action"] == "config-cluster-starter") {
           $templateSubDir = "Templates/ClusterStarter" ;  }
-      else {
+        else {
           $templateSubDir = "Templates/ClusterJoiner" ;  }
-      $templatesDir = str_replace("Model", $templateSubDir, dirname(__FILE__) ) ;
-      $templates = scandir($templatesDir);
-      foreach ($this->environments as $environment) {
-        foreach ($templates as $template) {
-          if (!in_array($template, array(".", ".."))) {
-              $templatorFactory = new \Model\Templating();
-              $templator = $templatorFactory->getModel($this->params);
-              $newFileName = str_replace("environment", $environment["any-app"]["gen_env_name"], $template ) ;
-              $autosDir = getcwd().'/build/config/cleopatra/cleofy/autopilots/generated';
-              $targetLocation = $autosDir.DIRECTORY_SEPARATOR.$newFileName ;
-              $templator->template(
-                  file_get_contents($templatesDir.DIRECTORY_SEPARATOR.$template),
-                  array(
-                      "cluster_address" => $this->getClusterStarter($environment["servers"]) ,
-                  ),
-                  $targetLocation );
-          echo $targetLocation."\n"; } } }
+        $templatesDir = str_replace("Model", $templateSubDir, dirname(__FILE__) ) ;
+
+        if (!isset($this->params["environment-name"])) {
+            $loggingFactory = new \Model\Logging() ;
+            $log = $loggingFactory->getModel($this->params) ;
+            $log->log("No environment name provided for Galera Cluster") ;
+            $this->params["environment-name"] = $this->askForEnvironment() ; }
+
+        $templatorFactory = new \Model\Templating();
+        $templator = $templatorFactory->getModel($this->params);
+        $targetLocation = "/etc/mysql/conf.d/wsrep.cnf" ;
+
+        $templator->template(
+            file_get_contents($templatesDir.DIRECTORY_SEPARATOR."wsrep.cnf"),
+            array(
+                "cluster_starter_target" => $this->getClusterStarter($this->getServersFromEnvName($this->params["environment-name"])) ,
+            ),
+            $targetLocation );
+        echo $targetLocation."\n";
     }
 
 }
