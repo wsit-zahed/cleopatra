695304759079bdf0e3a7d7e611818475341ce96a
David Amanshia <phpengine@hotmail.co.uk>
2014-08-28 16:33:24 +0100

    rackspace lists sizes, servers and images correctly

--- src/Modules/Rackspace/Views/RackspaceListView.tpl.php
+++ src/Modules/Rackspace/Views/RackspaceListView.tpl.php
@@ -1,64 +1,59 @@
 <?php
-if (is_object($pageVars["rackspaceResult"]) || is_array($pageVars["rackspaceResult"])) {
-    $arrayObject = new \ArrayObject($pageVars["rackspaceResult"]);
-    foreach ($arrayObject as $arrayObjectKey => $arrayObjectValue) {
-        $outVar = "" ;
-        if ($arrayObjectKey == "status") {
-            echo $arrayObjectKey.": $arrayObjectValue\n";  }
-        else if ($arrayObjectKey == "servers") {
-            foreach($arrayObjectValue as $serverEntry) {
-                $outVar .= "id - ".$serverEntry->id.", ";
-                $outVar .= "name - ".$serverEntry->name.", ";
-                $outVar .= "image_id - ".$serverEntry->image_id.", ";
-                $outVar .= "size_id - ".$serverEntry->size_id.", ";
-                $outVar .= "region_id - ".$serverEntry->region_id.", ";
-                $outVar .= "backups_active - ".$serverEntry->backups_active.", ";
-                $outVar .= "ip_address - ".$serverEntry->ip_address.", ";
-                $outVar .= "private_ip_address - ".$serverEntry->private_ip_address.", ";
-                $outVar .= "locked - ".$serverEntry->locked.", ";
-                $outVar .= "status - ".$serverEntry->status.", ";
-                $outVar .= "created_at - ".$serverEntry->created_at;
-                $outVar .= "\n" ; } }
-        else if ($arrayObjectKey == "sizes") {
-            foreach($arrayObjectValue as $sizeEntry) {
-                $outVar .= "id - ".$sizeEntry->id.", ";
-                $outVar .= "name - ".$sizeEntry->name.", ";
-                $outVar .= "slug - ".$sizeEntry->slug.", ";
-                $outVar .= "memory - ".$sizeEntry->memory.", ";
-                $outVar .= "cpu - ".$sizeEntry->cpu.", ";
-                $outVar .= "disk - ".$sizeEntry->disk.", ";
-                $outVar .= "cost_per_hour - ".$sizeEntry->cost_per_hour.", ";
-                $outVar .= "cost_per_month - ".$sizeEntry->cost_per_month;
-                $outVar .= "\n" ; } }
-        else if ($arrayObjectKey == "images") {
-            foreach($arrayObjectValue as $imageEntry) {
-                $outVar .= "id - ".$imageEntry->id.", ";
-                $outVar .= "name - ".$imageEntry->name.", ";
-                $outVar .= "distribution - ".$imageEntry->distribution.", ";
-                $outVar .= "slug - ".$imageEntry->slug.", ";
-                $outVar .= "public - ".$imageEntry->public ;
-                $outVar .= "\n" ; } }
-        else if ($arrayObjectKey == "domains") {
-            foreach($arrayObjectValue as $domainEntry) {
-                $outVar .= "id - ".$domainEntry->id.", ";
-                $outVar .= "name - ".$domainEntry->name.", ";
-                $outVar .= "ttl - ".$domainEntry->ttl.", ";
-                $outVar .= "live_zone_file - ".$domainEntry->live_zone_file.", ";
-                $outVar .= "zone_file_with_error - ".$domainEntry->zone_file_with_error ;
-                $outVar .= "\n" ; } }
-        else if ($arrayObjectKey == "regions") {
-            foreach($arrayObjectValue as $regionEntry) {
-                $outVar .= "id - ".$regionEntry->id.", ";
-                $outVar .= "name - ".$regionEntry->name.", ";
-                $outVar .= "slug - ".$regionEntry->slug;
-                $outVar .= "\n" ; } }
-        else if ($arrayObjectKey == "ssh_keys") {
-            foreach($arrayObjectValue as $sshKeyEntry) {
-                $outVar .= "id - ".$sshKeyEntry->id.", ";
-                $outVar .= "name - ".$sshKeyEntry->name ;
-                $outVar .= "\n" ; } }
-        echo $arrayObjectKey.":\n";
-        echo $outVar."\n" ; } }
+if (is_array($pageVars["rackspaceResult"])) {
+    $outVar = "" ;
+    $resultKeys = array_keys($pageVars["rackspaceResult"]);
+    $resultKey = $resultKeys[0];
+    // var_dump($pageVars["rackspaceResult"]);
+    if ($resultKey == "status") {
+        echo $resultKey.": {$pageVars["rackspaceResult"][$resultKey]}\n";  }
+    else if ($resultKey == "servers") {
+        foreach($pageVars["rackspaceResult"][$resultKey] as $serverEntry) {
+            $outVar .= "id - ".$serverEntry->id.", ";
+            $outVar .= "name - ".$serverEntry->name.", ";
+            $outVar .= "image_id - ".$serverEntry->image_id.", ";
+            $outVar .= "size_id - ".$serverEntry->size_id.", ";
+            $outVar .= "region_id - ".$serverEntry->region_id.", ";
+            $outVar .= "backups_active - ".$serverEntry->backups_active.", ";
+            $outVar .= "ip_address - ".$serverEntry->ip_address.", ";
+            $outVar .= "private_ip_address - ".$serverEntry->private_ip_address.", ";
+            $outVar .= "locked - ".$serverEntry->locked.", ";
+            $outVar .= "status - ".$serverEntry->status.", ";
+            $outVar .= "created_at - ".$serverEntry->created_at;
+            $outVar .= "\n" ; } }
+    else if ($resultKey == "sizes") {
+        foreach($pageVars["rackspaceResult"][$resultKey] as $sizeEntry) {
+            $outVar .= "id - ".$sizeEntry->id.", ";
+            $outVar .= "name - ".$sizeEntry->name.", ";
+            $outVar .= "memory - ".$sizeEntry->ram.", ";
+            $outVar .= "cpu count - ".$sizeEntry->vcpus.", ";
+            $outVar .= "disk - ".$sizeEntry->disk.", ";
+            $outVar .= "\n" ; } }
+    else if ($resultKey == "images") {
+        foreach($pageVars["rackspaceResult"][$resultKey] as $imageEntry) {
+            $outVar .= "name - ".$imageEntry->name.", ";
+            $outVar .= "id - ".$imageEntry->id ;
+            $outVar .= "\n" ; } }
+    else if ($resultKey == "domains") {
+        foreach($pageVars["rackspaceResult"][$resultKey] as $domainEntry) {
+            $outVar .= "id - ".$domainEntry->id.", ";
+            $outVar .= "name - ".$domainEntry->name.", ";
+            $outVar .= "ttl - ".$domainEntry->ttl.", ";
+            $outVar .= "live_zone_file - ".$domainEntry->live_zone_file.", ";
+            $outVar .= "zone_file_with_error - ".$domainEntry->zone_file_with_error ;
+            $outVar .= "\n" ; } }
+    else if ($resultKey == "regions") {
+        foreach($pageVars["rackspaceResult"][$resultKey] as $regionEntry) {
+            $outVar .= "id - ".$regionEntry->id.", ";
+            $outVar .= "name - ".$regionEntry->name.", ";
+            $outVar .= "slug - ".$regionEntry->slug;
+            $outVar .= "\n" ; } }
+    else if ($resultKey == "ssh_keys") {
+        foreach($pageVars["rackspaceResult"][$resultKey] as $sshKeyEntry) {
+            $outVar .= "id - ".$sshKeyEntry->id.", ";
+            $outVar .= "name - ".$sshKeyEntry->name ;
+            $outVar .= "\n" ; } }
+    echo $resultKey.":\n";
+    echo $outVar."\n" ; }
 else {
     echo "There was a problem listing Data. No results were found"; }
 ?>
