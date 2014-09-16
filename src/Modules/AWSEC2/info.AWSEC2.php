5acb067c8439b56df3be64b00695d83c748b84d2
David Amanshia <phpengine@hotmail.co.uk>
2014-09-16 10:36:16 +0100

    added to help

--- src/Modules/AWSEC2/info.AWSEC2.php
+++ src/Modules/AWSEC2/info.AWSEC2.php
@@ -31,6 +31,21 @@ class AWSEC2Info extends CleopatraBase {
 
     AWSEC2, awsec2, aws-ec2
 
+        - box-add
+        Lets you add boxes to Rackspace, and adds them to your papyrusfile
+        example: cleopatra aws-ec2 box-add
+                    --yes
+                    --aws-ssh-key-path="/home/dave/.ssh/bastion.pub"
+                    --aws-ssh-key-name="bastion"
+
+        - box-destroy
+        Will destroy box/es in an environment for you, and remove them from the papyrus file
+        example: cleopatra aws-ec2 box-destroy --yes --guess --aws-ssh-key-path="/home/dave/.ssh/bastion.pub" --aws-ssh-key-name="bastion"
+
+        - box-destroy-all
+        Will destroy all boxes in your Rackspace account - Careful - its irreversible
+        example: cleopatra aws-ec2 box-destroy-all --yes --guess
+
         - save-ssh-key
         Will let you save a local ssh key to your AWS EC2 account, so you can ssh in to your nodes
         securely and without a password
@@ -39,6 +54,9 @@ class AWSEC2Info extends CleopatraBase {
         - list
         Will display data about your digital ocean account
         example: dapperstrano aws-ec2 list
+
+        Note: region must be one of the following...
+        us-east-1, ap-northeast-1, sa-east-1, ap-southeast-1, ap-southeast-2, us-west-2, us-gov-west-1, us-west-1, cn-north-1, eu-west-1
 
 HELPDATA;
       return $help ;
