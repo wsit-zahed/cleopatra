62ef36175e4b788c5eb629449587a1b8462e17f2
David Amanshia <phpengine@hotmail.co.uk>
2014-09-16 22:41:16 +0100

    more detailed user output on success or failure

--- src/Modules/User/Views/UserView.tpl.php
+++ src/Modules/User/Views/UserView.tpl.php
@@ -2,10 +2,19 @@ User Modifications:
 --------------------------------------------
 
 <?php echo $pageVars["appName"] ; ?>: <?php
-  $result_summary = ($pageVars["result"] == true) ? "Success" : "Failure" ;
-  echo $result_summary."\n" ;
-  if (is_string($pageVars["result"])) {
-    echo $pageVars["result"]."\n" ; }
+
+    if ($pageVars["pageVars"]["route"]["action"] == "exists") {
+        $result_summary = ($pageVars["result"] == true) ? "Success = User Exists" : "Failure - User Does Not Exist" ;
+        $user_name = "" ;
+        $user_name = (isset($pageVars["params"]["user-name"])) ? $pageVars["params"]["user-name"] : $user_name ;
+        $user_name = (isset($pageVars["params"]["username"])) ? $pageVars["params"]["username"] : $user_name ;
+        echo $result_summary."\n" ;
+        echo "User Name: ".$user_name."\n" ; }
+    else if (is_string($pageVars["result"])) {
+        echo $pageVars["result"]."\n" ; }
+    else {
+        echo $pageVars["result"]."\n" ; }
+
 ?>
 
 ------------------------------
