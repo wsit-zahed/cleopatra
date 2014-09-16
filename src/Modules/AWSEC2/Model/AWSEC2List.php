6ca729d0139ad858bcc81db5763f70ed3a3b6341
David Amanshia <phpengine@hotmail.co.uk>
2014-09-16 10:35:23 +0100

    list returns data, but there are 12500 images on aws so it takes ages

--- src/Modules/AWSEC2/Model/AWSEC2List.php
+++ src/Modules/AWSEC2/Model/AWSEC2List.php
@@ -20,31 +20,50 @@ class AWSEC2List extends BaseAWSEC2AllOS {
 
     protected function performAWSEC2ListData($params=null){
         if ($this->askForListExecute() != true) { return false; }
-        $this->apiKey = $this->askForAWSEC2APIKey();
-        $this->clientId = $this->askForAWSEC2ClientID();
+        $this->initialiseAWS();
         $dataToList = $this->askForDataTypeToList();
         return $this->getDataListFromAWSEC2($dataToList);
     }
 
-    private function askForListExecute(){
+    protected function askForListExecute(){
         if (isset($this->params["yes"]) && $this->params["yes"]==true) { return true ; }
         $question = 'List Data?';
         return self::askYesOrNo($question);
     }
 
-    private function askForDataTypeToList(){
+    protected function getClient() {
+        $this->setProjVars() ;
+        $c = array(
+            'key'    => $this->accessKey,
+            'secret' => $this->secretKey,
+            'region' => $this->region ) ;
+        $this->awsClient = \Aws\Ec2\Ec2Client::factory($c);
+    }
+
+    protected function askForDataTypeToList(){
         $question = 'Please choose a data type to list:';
         $options = array("droplets", "sizes", "images", "domains", "regions", "ssh_keys");
-        if (isset($this->params["aws-ec2-list-data-type"]) &&
-            in_array($this->params["aws-ec2-list-data-type"], $options)) {
-            return $this->params["aws-ec2-list-data-type"] ; }
+        if (isset($this->params["list-type"]) &&
+            in_array($this->params["list-type"], $options)) {
+            return $this->params["list-type"] ; }
         return self::askForArrayOption($question, $options, true);
     }
 
     public function getDataListFromAWSEC2($dataToList){
-        $callVars = array();
-        $curlUrl = "https://api.awsec2.com/$dataToList/" ;
-        return $this->awsCall($callVars, $curlUrl);
+        $calls = array(
+            "images" => "DescribeImages",
+        ) ;
+        $method = $calls[$dataToList] ;
+        $list = $this->awsClient->$method()  ;
+
+
+        $iterator = $this->awsClient->getIterator($method, array( ), array(
+            'limit'     => 10,
+            'page_size' => 10
+        ));
+
+
+        return $iterator ;
     }
 
 }
