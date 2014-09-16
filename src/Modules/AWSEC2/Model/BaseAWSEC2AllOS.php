a73c3a44443ace90d572c7f40b32a50187323fd2
David Amanshia <phpengine@hotmail.co.uk>
2014-09-16 10:35:45 +0100

    authenticated properly for aws base class

--- src/Modules/AWSEC2/Model/BaseAWSEC2AllOS.php
+++ src/Modules/AWSEC2/Model/BaseAWSEC2AllOS.php
@@ -14,11 +14,20 @@ class BaseAWSEC2AllOS extends Base {
     // Model Group
     public $modelGroup = array("Base") ;
 
-    protected $clientId ;
-    protected $apiKey ;
+    protected $accessKey ;
+    protected $secretKey ;
+    protected $region ;
+    protected $awsClient ;
+
+    protected function initialiseAWS() {
+        $this->loadLibs();
+        $this->accessKey = $this->askForAWSAccessKey();
+        $this->secretKey = $this->askForAWSSecretKey();
+        $this->region = $this->askForAWSRegion();
+        $this->getClient();
+    }
 
     public function __construct($params) {
-        $this->loadLibs();
         parent::__construct($params);
     }
 
@@ -28,70 +37,74 @@ class BaseAWSEC2AllOS extends Base {
         require_once($pharFile) ;
     }
 
-    protected function askForAWSEC2AccessKey(){
-        if (isset($this->params["aws-ec2-access-key"])) {
-            putenv("AWS_ACCESS_KEY_ID={$this->params["aws-ec2-access-key"]}") ;
-            return $this->params["aws-ec2-access-key"] ; }
-        $papyrusVar = \Model\AppConfig::getProjectVariable("aws-ec2-access-key") ;
+    protected function askForAWSAccessKey(){
+        if (isset($this->params["aws-access-key"])) {
+            return $this->params["aws-access-key"] ; }
+        $papyrusVar = \Model\AppConfig::getProjectVariable("aws-access-key") ;
         if ($papyrusVar != null) {
+            if ($this->params["guess"] == true) { return $papyrusVar ; }
             $question = 'Use Project saved AWS Access Key?';
             if (self::askYesOrNo($question, true) == true || $this->params["yes"] == true) {
-                putenv("AWS_ACCESS_KEY_ID=$papyrusVar") ;
                 return $papyrusVar ; } }
-        $appVar = \Model\AppConfig::getProjectVariable("aws-ec2-access-key") ;
+        $appVar = \Model\AppConfig::getProjectVariable("aws-access-key") ;
         if ($appVar != null) {
             $question = 'Use Application saved AWS Access Key?';
             if (self::askYesOrNo($question, true) == true || $this->params["yes"] == true) {
-                putenv("AWS_ACCESS_KEY_ID=$appVar") ;
                 return $appVar ; } }
         $question = 'Enter AWS Access Key';
         $key = self::askForInput($question, true);
-        putenv("AWS_ACCESS_KEY_ID=$key") ;
         return $key ;
     }
 
-    protected function askForAWSEC2SecretKey(){
-        if (isset($this->params["aws-ec2-secret-key"])) {
-            putenv("AWS_SECRET_KEY={$this->params["aws-ec2-secret-key"]}") ;
-            return $this->params["aws-ec2-secret-key"] ; }
-        $papyrusVar = \Model\AppConfig::getProjectVariable("aws-ec2-secret-key") ;
+    protected function askForAWSSecretKey(){
+        if (isset($this->params["aws-secret-key"])) {
+            return $this->params["aws-secret-key"] ; }
+        $papyrusVar = \Model\AppConfig::getProjectVariable("aws-secret-key") ;
         if ($papyrusVar != null) {
+            if ($this->params["guess"] == true) { return $papyrusVar ; }
             $question = 'Use Project saved AWS EC2 Client ID?';
             if (self::askYesOrNo($question, true) == true) {
-                putenv("AWS_SECRET_KEY=$papyrusVar") ;
                 return $papyrusVar ; } }
-        $appVar = \Model\AppConfig::getProjectVariable("aws-ec2-secret-key") ;
+        $appVar = \Model\AppConfig::getProjectVariable("aws-secret-key") ;
         if ($appVar != null) {
             $question = 'Use Application saved AWS EC2 Client ID?';
             if (self::askYesOrNo($question, true) == true) {
-                putenv("AWS_SECRET_KEY=$appVar") ;
                 return $appVar ; } }
         $question = 'Enter AWS EC2 Secret Key';
         return self::askForInput($question, true);
     }
 
-    protected function awsCall(Array $curlParams, $curlUrl){
-        $curlParams["client_id"] = $this->clientId ;
-        $curlParams["api_key"] = $this->apiKey;
-        \Model\AppConfig::setProjectVariable("aws-ec2-client-id", $this->clientId) ;
-        \Model\AppConfig::setProjectVariable("aws-ec2-api-key", $this->apiKey) ;
-        $postQuery = "";
-        $i = 0;
-        foreach ($curlParams as $curlParamKey => $curlParamValue) {
-            $postQuery .= ($i==0) ? "" : '&' ;
-            if(is_object($curlParamValue)) {
-                var_dump($curlParamKey, $curlParamValue) ;  }
-            $postQuery .= $curlParamKey."=".$curlParamValue;
-            $i++; }
-        // echo $curlUrl.'?'.$postQuery ;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $curlUrl.'?'.$postQuery);
-        // receive server response ...
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $server_output = curl_exec ($ch);
-        curl_close ($ch);
-        $callObject = json_decode($server_output);
-        return $callObject;
+    protected function askForAWSRegion(){
+        if (isset($this->params["aws-region"])) { return $this->params["aws-region"] ; }
+        $papyrusVar = \Model\AppConfig::getProjectVariable("aws-region") ;
+        if ($papyrusVar != null) {
+            if ($this->params["guess"] == true) { return $papyrusVar ; }
+            if ($this->params["use-project-region"] == true) { return $papyrusVar ; }
+            $question = 'Use Project saved AWS Region?';
+            if (self::askYesOrNo($question, true) == true) {
+                return $papyrusVar ; } }
+        $appVar = \Model\AppConfig::getProjectVariable("aws-region") ;
+        if ($appVar != null) {
+            $question = 'Use Application saved AWS Region?';
+            if (self::askYesOrNo($question, true) == true) {
+                return $appVar ; } }
+        $question = 'Enter AWS Region';
+        return self::askForInput($question, true);
+    }
+
+    protected function getServerGroupRegionID() {
+        if (isset($this->params["region-id"])) {
+            return $this->params["region-id"] ; }
+        if (isset($this->params["guess"])) {
+            return $this->region ; }
+        $question = 'Enter Region ID for this Server Group';
+        return self::askForInput($question, true);
+    }
+
+    protected function setProjVars() {
+        \Model\AppConfig::setProjectVariable("aws-secret-key", $this->secretKey) ;
+        \Model\AppConfig::setProjectVariable("aws-access-key", $this->accessKey) ;
+        \Model\AppConfig::setProjectVariable("aws-region", $this->region) ;
     }
 
 
